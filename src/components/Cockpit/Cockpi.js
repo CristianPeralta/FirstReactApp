2515143e7664c5238ceb3c5495f9c180bf91c498
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-06 22:09:20 -0500

    build Cockpit component

---
+++ src/components/Cockpit/Cockpi.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import classes from './Cockpit.css';
+
+const cockpit = (props) => {
+    let btnClass = '';
+    if (props.showBooks) {
+        btnClass = classes.Red;
+    }
+    const assignedClasses = [];
+    if (props.books.length >= 2) {
+      assignedClasses.push(classes.green);
+    } else if (props.books.length < 2) {
+      assignedClasses.push(classes.bold);
+    }
+
+    return (
+        <div className={classes.Cockpit}>
+            <h1 className={assignedClasses.join(' ')}>Hey Guys</h1>
+            <button className={btnClass} onClick={props.clicked} >Show Books</button>
+        </div>
+    );
+}
+export default cockpit;
