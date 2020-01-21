f11cddff7fe330827377a58f2f87ff28f45f1b7b
CristianPeralta <cbperaltasegura@gmail.com>
2020-01-21 09:55:42 -0500

    render adjacent jsx elements for Books ompononent

--- src/components/Books/Book/Book.js
+++ src/components/Books/Book/Book.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
 import classes from './Book.css';
+import Aux from "../../hoc/Auxiliary";
 
 class Book extends Component {
     render() {
-        console.log("[Book.js] rendering")
+        console.log("[Book.js] rendering");
         return (
-            <div className={classes.Book}>
+            <Aux>
                 <p onClick={this.props.click} >This book called {this.props.title} cost {this.props.price}</p>
                 <p>{this.props.children}</p>
                 <input type="text" onChange={this.props.changed} value={this.props.title} />
-            </div>
+            </Aux>
         );
     }
 }
